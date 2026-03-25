584b4ee3324cd0a1bbc080ac7e84eea7d548f0f9
Lovekumar-10 <Lovekumar161129@gmail.com>
2026-03-25 21:04:23 +0530

    update

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -65,10 +65,12 @@
 
 
 
+
+
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
-import PublicRoutes from "./publicRoutes/PublicRoutes"
+import PublicRoutes from "./publicRoutes/PublicRoutes";
 import AuthRoutes from "./auth/AuthRoutes"
 import DashboardRoutes from "./dashboardRoutes/DashboardRoutes";
 
@@ -87,14 +89,6 @@ const AppRoutes = () => {
 
       </Routes>
     </Router>
-  );
-};
-
-export default AppRoutes;
-
-
-
-
   );
 };
 
